8fffb5d98ee26b13fcccb658e044d788def00845
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 14:37:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(array1)
     if block_given?
-      counter = 0 
-      while counter < array1.length 
-        yield array1[counter]
-        counter += 1 
+      i = 0 
+      while i < array1.length 
+        yield array1[i]
+        i = i + 1 
       end
       array1
     else
