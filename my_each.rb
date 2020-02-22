624c69e6cf2b6ea476b4025b0dd09f9fcc364e67
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 14:57:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,10 @@
 def my_each(array)
-    if block_given?
-      i = 1
-      while i < array.length
-        yield array[i]
-        i = i + 1
-      end
-    array
-    else
-      puts "Hey! No block was given!"
+    i = 1
+    while i < array.length
+      yield array[i]
+      i = i + 1
     end
+  array
 end
 
 my_each(array) do |word|
