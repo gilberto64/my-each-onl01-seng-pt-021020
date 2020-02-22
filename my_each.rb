60ef744bb646b25bf38a5a524f381237a0496212
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 15:01:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,9 @@
 def my_each(array)
-    i = 1
-    while i < array.length
-      yield array[i]
-      i = i + 1
-    end
-  array
+   # i = 0 
+    #  while i < array.length
+     # yield array[i]
+    #  i = i + 1
+    #end
+  # array
 end
 
-
-
-my_each(Array) do |word|
-  puts "This is #{word}"
-end
